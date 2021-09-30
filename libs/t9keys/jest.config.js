9b8d6075ba0e82aeed0acf7b0c6ade79f1ce0b05
sam <lad.sangram@gmail.com>
2021-09-30 11:10:37 +0100

    initial commit

---
+++ libs/t9keys/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 't9keys',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/t9keys',
+};
---
+++ libs/t9keys/src/lib/t9keys.spec.ts
@@ -0,0 +1,7 @@
+import { t9keys } from './t9keys';
+
+describe('t9keys', () => {
+  it('should work', () => {
+    expect(t9keys()).toEqual('t9keys');
+  });
+});
